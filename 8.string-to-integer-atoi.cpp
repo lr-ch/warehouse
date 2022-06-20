0583592c2630cb1185080fd713b2371e2d929db5
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-06-21 02:45:49 +0800

    8.string-to-integer-atoi.cpp

---
+++ 8.string-to-integer-atoi.cpp
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=8 lang=cpp
+ *
+ * [8] String to Integer (atoi)
+ */
+
+// @lc code=start
+class Solution {
+	bool isDigit(char c) {
+		return (c >= '0' && c <= '9') ? true : false;
+	}
+	bool isAlpha(char c) {
+		return ((c >= 'a' && c <= 'z') ||
+				(c >= 'A' && c <= 'Z')) ? true : false;
+	}
+	bool isSpace(char c) {
+		return c == ' ' ? true : false;
+	}
+public:
+	int myAtoi(string s) {
+		long double ans = 0;
+		long double sign = 1;
+		for (int i = 0; i < s.length(); i++) {
+			if (isDigit(s[i])) {
+				ans = ans * 10 + (s[i] - '0');
+			} else if (s[i] == '-' && i + 1 < s.length() && isDigit(s[i + 1])) {
+				sign = -1;
+				if (i - 1 >= 0 && isDigit(s[i - 1]))
+					break;
+			} else if (s[i] == '+' && i + 1 < s.length() && isDigit(s[i + 1])) {
+				if (i - 1 >= 0 && isDigit(s[i - 1]))
+					break;
+			} else if (isAlpha(s[i])) {
+				break;
+			} else if (isSpace(s[i])) {
+				if (i - 1 >= 0 && isDigit(s[i - 1]))
+					break;
+				else
+					continue;
+			} else
+				break;
+			}
+
+		if (ans * sign > INT_MAX) return INT_MAX;
+		if (ans * sign < INT_MIN) return INT_MIN;
+		return ans * sign;
+	}
+};
+// @lc code=end
+
