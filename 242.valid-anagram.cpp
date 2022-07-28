94e474e9114890d7d1e31c2a62484e403d6f6511
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-07-28 22:56:50 +0800

    * daily challenge (2022/07/28)   Add a new solution    Accepted      * 36/36 cases passed (18 ms)      * Your runtime beats 40.58 % of cpp submissions      * Your memory usage beats 45.59 % of cpp submissions (7.4 MB)

--- 242.valid-anagram.cpp
+++ 242.valid-anagram.cpp
@@ -6,7 +6,13 @@
 
 // @lc code=start
 class Solution {
-public:
+	/*
+	 * Accepted
+	 *   36/36 cases passed (14 ms)
+	 *   Your runtime beats 57.46 % of cpp submissions
+	 *   Your memory usage beats 7.79 % of cpp submissions (7.5 MB)
+	 */
+/*
 	bool isAnagram(string s, string t) {
 		map<char, int> ms;
 
@@ -26,6 +32,28 @@ public:
 				return false;
 
 		return true;
+	}
+*/
+public:
+	/*
+	 * Accepted
+	 *   36/36 cases passed (18 ms)
+	 *   Your runtime beats 40.58 % of cpp submissions
+	 *   Your memory usage beats 45.59 % of cpp submissions (7.4 MB)
+	 */
+	bool isAnagram(string s, string t) {
+        if (s.size() < t.size()) return false;
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        for (int i = 0; i < t.size(); i++)
+            if (s[i] != t[i])
+                return false;
+            else
+                s[i] = 0;
+
+        return accumulate(s.begin(), s.end(), 0) == 0;
 	}
 };
 // @lc code=end
