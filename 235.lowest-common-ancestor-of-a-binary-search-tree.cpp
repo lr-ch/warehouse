52f14c0252c6f6c95067bc48b4e5bd074e1c5cfe
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-08-12 21:52:13 +0800

    * daily challenge (2022/08/12) * Add iteration solution   Accepted    * 28/28 cases passed (30 ms)    * Your runtime beats 94.63 % of cpp submissions    * Your memory usage beats 12.34 % of cpp submissions (23.3 MB)

--- 235.lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235.lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -17,6 +17,14 @@
 
 class Solution {
 public:
+	/*
+	 * -- Recursive
+	 * Accepted
+	 *  * 27/27 cases passed (33 ms)
+	 *  * Your runtime beats 87.54 % of cpp submissions
+	 *  * Your memory usage beats 12.41 % of cpp submissions (23.3 MB)
+	 */
+/*
 	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
 		if (!root) return root;
 
@@ -26,6 +34,23 @@ public:
 		if (root == p || root == q) return root;
 		if (!left) return right;
 		if (!right) return left;
+
+		return root;
+	}
+*/
+	/*
+	 * -- Iteration
+	 * Accepted
+	 *  * 28/28 cases passed (30 ms)
+	 *  * Your runtime beats 94.63 % of cpp submissions
+	 *  * Your memory usage beats 12.34 % of cpp submissions (23.3 MB)
+	 */
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+		int lo = min(p->val, q->val);
+		int hi = max(p->val, q->val);
+
+		while (root->val < lo || root->val > hi)
+			root = root->val < lo ? root->right : root->left;
 
 		return root;
 	}
