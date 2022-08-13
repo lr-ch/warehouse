7eec0be1ade845d88ed9309f27a9f1bf4a8cd7a7
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-08-14 02:02:07 +0800

    * daily challenge (2022/08/13)   Accepted    * 178/178 cases passed (895 ms)    * Your runtime beats 34.58 % of cpp submissions    * Your memory usage beats 22.42 % of cpp submissions (290.3 MB)

---
+++ 30.substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=30 lang=cpp
+ *
+ * [30] Substring with Concatenation of All Words
+ */
+
+// @lc code=start
+class Solution {
+public:
+	vector<int> findSubstring(string s, vector<string>& words) {
+		if (s == "" || words.size() == 0) return {};
+
+		int word_len = words[0].length();	// all words have the same length
+		int word_count = words.size();
+
+		// if s is shorter than total length of words
+		if (word_len * word_count > s.size()) return {};
+
+		map<string, int> dict;				// [word, count]
+		vector<int> ans;
+
+		for (auto& w : words) dict[w]++;	// words may contains repeated word
+
+		for (int i = 0; i <= s.size() - word_len * word_count; i++) {
+			map<string, int> copy = dict;
+
+			int j = i;
+			int count = word_count;
+
+			while (count > 0) {
+				string comp_str = s.substr(j, word_len);
+				// if the string is not in dict
+				if (copy.count(comp_str) == 0)
+					break;
+
+				// if the word appears too many times
+				if (--copy[comp_str] < 0)
+					break;
+
+				count--;
+				j += word_len;
+			}
+			if (count == 0) ans.push_back(i);
+		}
+
+		return ans;
+	}
+};
+// @lc code=end
