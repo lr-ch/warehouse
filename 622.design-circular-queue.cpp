313d7a1a3f89b3dbe69a5cdb6c67d97865a0f106
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-09-25 23:53:40 +0800

    * daily challenge (2022/09/25)   - Add an implementation by array   - Accepted      * 58/58 cases passed (33 ms)      * Your runtime beats 71.04 % of cpp submissions      * Your memory usage beats 48.83 % of cpp submissions (16.8 MB)

--- 622.design-circular-queue.cpp
+++ 622.design-circular-queue.cpp
@@ -5,19 +5,17 @@
  */
 
 // @lc code=start
-class MyCircularQueue {
+class MyCircularQueueLinkedList {
 	struct Node {
 		Node* next;
 		int val;
-		Node() {}
-		~Node() {}
 	};
 
 	Node* front = nullptr;
 	Node* rear = nullptr;
 	int qsize = 0, cursize = 0;
 public:
-	MyCircularQueue(int k) {
+	void init(int k) {
 		qsize = k;
 		front = new Node[k];
 		rear = front;
@@ -66,6 +64,96 @@ public:
 
 	bool isFull() {
 		return cursize == qsize;
+	}
+};
+
+class MyCircularQueueArray {
+	vector<int> q;
+	int qsize = 0, front = 0;
+public:
+	void init(int k) {
+		q.resize(k);
+	}
+
+	bool enQueue(int value) {
+		if (!isFull()) {
+			q[(front + qsize) % q.size()] = value;
+			qsize++;
+			return true;
+		}
+		return false;
+	}
+
+	bool deQueue() {
+		if (!isEmpty()) {
+			front = (front + 1) % q.size();
+			qsize--;
+			return true;
+		}
+		return false;
+	}
+
+	int Front() {
+		return isEmpty() ? -1 : q[front];
+	}
+
+	int Rear() {
+		return isEmpty() ? -1 : q[(front + qsize - 1) % q.size()];
+	}
+
+	bool isEmpty() {
+		return qsize == 0;
+	}
+
+	bool isFull() {
+		return qsize == q.size();
+	}
+};
+
+class MyCircularQueue {
+/*
+ * Accepted
+ *  - 58/58 cases passed (33 ms)
+ *  - Your runtime beats 71.04 % of cpp submissions
+ *  - Your memory usage beats 48.83 % of cpp submissions (16.8 MB)
+ */
+	MyCircularQueueArray		solution;
+
+/*
+ * Accepted
+ *  - 58/58 cases passed (71 ms)
+ *  - Your runtime beats 5.11 % of cpp submissions
+ *  - Your memory usage beats 11.06 % of cpp submissions (17 MB)
+ */
+//	MyCircularQueueLinkedList	solution;
+
+public:
+	MyCircularQueue(int k) {
+		solution.init(k);
+	}
+
+	bool enQueue(int value) {
+		return solution.enQueue(value);
+	}
+
+	bool deQueue() {
+		return solution.deQueue();
+	}
+
+	int Front() {
+		return solution.Front();
+	}
+
+	int Rear() {
+		return solution.Rear();
+	}
+
+	bool isEmpty() {
+		return solution.isEmpty();
+	}
+
+	bool isFull() {
+		return solution.isFull();
 	}
 };
 
