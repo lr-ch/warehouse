6824055f22f3ece20febafdf17346eceb4dc5139
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-10-17 00:02:50 +0800

    * Add a iteration solution   Accepted    * 34/34 cases passed (47 ms)    * Your runtime beats 81.63 % of cpp submissions    * Your memory usage beats 83.12 % of cpp submissions (7.4 MB)

--- 1335.minimum-difficulty-of-a-job-schedule.cpp
+++ 1335.minimum-difficulty-of-a-job-schedule.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class Recursive {
 	int dp(vector<int>& jobs, int splitIndex, int remainDays) {
 		// *** You have to finish at least one task every day. ***
 		if (remainDays <= 1) return *max_element(jobs.begin() + splitIndex, jobs.end());
@@ -27,11 +27,49 @@ class Solution {
 	// minimun difficulty cost scheduled first splitIndex jobs in remainDays
 	array<array<int, 11>, 301> memo;
 public:
-	int minDifficulty(vector<int>& jobDifficulty, int d) {
+	int solve(vector<int>& jobDifficulty, int d) {
 		if (d > jobDifficulty.size()) return -1;
 
 		for (auto& m : memo) fill(m.begin(), m.end(), -1);
 		return dp(jobDifficulty, 0, d);
+	}
+};
+
+class Iterate {
+	array<array<int, 11>, 301> memo;
+public:
+	int solve(vector<int>& jobDifficulty, int d) {
+		if (jobDifficulty.size() < d) return -1;
+
+		for (auto& m : memo) fill(m.begin(), m.end(), 3e6);
+
+		/*
+		 *    k - 1            1
+		 * +------------+-------------+
+		 * | 1 ...... j | j + 1 ... i |
+		 * +------------+-------------+
+		 *   dp[j][k-1]   max(jobs[j+1], jobs[j+2], ..., jobs[i])
+		 */
+		memo[0][0] = 0;
+		for (int i = 1; i <= jobDifficulty.size(); i++) {
+			for (int k = 1; k <= d; k++) {
+				int max_difficulty = 0;					// track for max(jobs[j+1], jobs[j+2], ..., jobs[i])
+				for (int j = i - 1; j >= k - 1; j--) {
+					max_difficulty = max(max_difficulty, jobDifficulty[j]);
+					memo[i][k] = min(memo[i][k], max_difficulty + memo[j][k - 1]);
+				}
+			}
+		}
+		return memo[jobDifficulty.size()][d];
+	}
+};
+
+class Solution {
+	Recursive recur;
+	Iterate itera;
+public:
+	int minDifficulty(vector<int>& jobDifficulty, int d) {
+		return itera.solve(jobDifficulty, d);
 	}
 };
 // @lc code=end
