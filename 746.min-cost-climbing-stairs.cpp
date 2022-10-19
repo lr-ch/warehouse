48745ed0107b952a07cddd6441b020d1e510a5c5
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-10-19 22:21:12 +0800

    * Add a solution of iteration   Accepted    - 283/283 cases passed (15 ms)    - Your runtime beats 40.26 % of cpp submissions    - Your memory usage beats 54.85 % of cpp submissions (13.8 MB)

--- 746.min-cost-climbing-stairs.cpp
+++ 746.min-cost-climbing-stairs.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class SolutionRecur {
 	int dp(vector<int>& cost, int i, vector<int>& memo) {
 		if (i > cost.size() - 1) return 0;
 		if (memo[i] != -1) return memo[i];
@@ -23,6 +23,31 @@ public:
 		// Constraints:
 		//   2 <= cost.length <= 1000
 		return min(dp(cost, 0, memo), dp(cost, 1, memo));
+	}
+};
+
+class SolutionItera {
+public:
+	int minCostClimbingStairs(vector<int>& cost) {
+		int n = cost.size();
+		vector<int> memo(n, 0);
+
+		memo[0] = cost[0];
+		memo[1] = cost[1];	// memo[1] = min(cost[0] + cost[1], cost[1])
+
+		for (int i = 2; i < n; i++)
+			memo[i] = cost[i] + min(memo[i - 1], memo[i - 2]);
+		return min(memo[n - 1], memo[n - 2]);
+	}
+};
+
+class Solution {
+	SolutionRecur recur;
+	SolutionItera itera;
+public:
+	int minCostClimbingStairs(vector<int>& cost) {
+	//	return recur.minCostClimbingStairs(cost);
+		return itera.minCostClimbingStairs(cost);
 	}
 };
 // @lc code=end
