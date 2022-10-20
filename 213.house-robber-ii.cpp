5be8528c82c358a3c5a1babd1013b0507c4b29de
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-10-21 00:58:10 +0800

    * Improved sub-function for genericly processing start/end house   Accepted    * 75/75 cases passed (0 ms)    * Your runtime beats 100 % of cpp submissions    * Your memory usage beats 63.37 % of cpp submissions (7.8 MB)

--- 213.house-robber-ii.cpp
+++ 213.house-robber-ii.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class SolutionPassByValue {
 	int dp(vector<int> n) {
 		fill(memo.begin(), memo.end(), -1);
 
@@ -26,6 +26,40 @@ public:
 		int startFromFirstHouse = dp(vector<int>(nums.begin(), nums.end() - 1));
 		int startFromSecondHouse = dp(vector<int>(nums.begin() + 1, nums.end()));
 		return max(startFromFirstHouse, startFromSecondHouse);
+	}
+};
+
+class SolutionPassByReference {
+	int dp(const vector<int>& n, int start, int end) {
+		fill(memo.begin(), memo.end(), -1);
+
+		if (start == end) return n[start];
+
+		memo[start] = n[start];
+		memo[start + 1] = max(memo[start], n[start + 1]);
+
+		for (int i = start + 2; i <= end; i++)
+			memo[i] = max(memo[i - 2] + n[i], memo[i - 1]);
+		return memo[end];
+	}
+
+	array<int, 101> memo;
+public:
+	int rob(vector<int>& nums) {
+		if (nums.size() < 2) return nums[0];
+		int startFromFirstHouse = dp(nums, 0, nums.size() - 2);
+		int startFromSecondHouse = dp(nums, 1, nums.size() - 1);
+		return max(startFromFirstHouse, startFromSecondHouse);
+	}
+};
+
+class Solution {
+	SolutionPassByValue pv;
+	SolutionPassByReference pr;
+public:
+	int rob(vector<int>& nums) {
+	//	return pv.rob(nums);
+		return pr.rob(nums);
 	}
 };
 // @lc code=end
