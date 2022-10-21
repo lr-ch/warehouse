cce05fb1a284d2b94e282587a7f50ddfde9fcee5
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-10-21 09:27:10 +0800

    * daily challenge (2022/10/21) * Add a sliding window solution    Accepted     - 52/52 cases passed (254 ms)     - Your runtime beats 77.79 % of cpp submissions     - Your memory usage beats 15.96 % of cpp submissions (80.4 MB)

--- 219.contains-duplicate-ii.cpp
+++ 219.contains-duplicate-ii.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class SolutionHashMap {
 public:
 	bool containsNearbyDuplicate(vector<int>& nums, int k) {
 		unordered_map<int, vector<int>> table;	// <number, indices of the number>
@@ -18,6 +18,37 @@ public:
 					if (indices[j + 1] - indices[j] <= k)
 						return true;
 		return false;
+	}
+};
+
+class SolutionSlidingWindow {
+	set<int> window;
+public:
+	bool containsNearbyDuplicate(vector<int>& nums, int k) {
+		int left = 0, right = 0;
+
+		for (; right < nums.size(); right++) {
+			if (right - left > k) {
+				window.erase(nums[left]);
+				left++;
+			}
+
+			if (window.find(nums[right]) != window.end())
+				return true;
+
+			window.insert(nums[right]);
+		}
+		return false;
+	}
+};
+
+class Solution {
+	SolutionHashMap hash_map;
+	SolutionSlidingWindow sliding_window;
+public:
+	bool containsNearbyDuplicate(vector<int>& nums, int k) {
+	//	return hash_map.containsNearbyDuplicate(nums, k);
+		return sliding_window.containsNearbyDuplicate(nums, k);
 	}
 };
 // @lc code=end
