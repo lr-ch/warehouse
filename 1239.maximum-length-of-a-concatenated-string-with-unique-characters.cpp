3bed2d2a5c8cd7441b4d5bfd4ada6dee8be2f15c
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-10-25 01:22:44 +0800

    * Replace std::set<> with std::bitset<> to massively improve efficiency * daily challenge (2022/10/24)   Accepted    * 89/89 cases passed (9 ms)    * Your runtime beats 88.33 % of cpp submissions    * Your memory usage beats 77.45 % of cpp submissions (9.6 MB)

--- 1239.maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239.maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class SolutionSet {
 	bool isConcatable(set<char>& a, set<char>& b) {
 		for (auto& ch : a) if (b.count(ch) > 0) return false;
 		return true;
@@ -41,6 +41,34 @@ public:
 					concatenation.back().insert(f.begin(), f.end());
 					// update answer
 					maxlen = max(maxlen, concatenation.back().size());
+				}
+			}
+		}
+
+		return maxlen;
+	}
+};
+
+class Solution {
+public:
+	int maxLength(vector<string>& arr) {
+		vector<bitset<26>> unibits;
+
+		for (auto& str : arr) {
+			bitset<26> tmp;
+			for (auto& ch : str) tmp.set(ch - 'a');
+
+			// if there is no duplicate characters in *str*
+			if (str.length() == tmp.count()) unibits.push_back(tmp);
+		}
+
+		vector<bitset<26>> concat(1, 0);
+		size_t maxlen = 0;
+		for (auto& bits : unibits) {
+			for (int i = 0; i < concat.size(); i++) {
+				if ((concat[i] & bits).none()) {
+					concat.push_back(bits | concat[i]);
+					maxlen = max(concat.back().count(), maxlen);
 				}
 			}
 		}
