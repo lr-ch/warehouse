483c0f711dcee361fcf94ac93bd97cf37a892643
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-10-28 00:41:44 +0800

    * daily challenge (2022/10/27) * Added a new solution by mapping 1's corrdination from img1 to img2   Accepted    - 59/59 cases passed (265 ms)    - Your runtime beats 35.42 % of cpp submissions    - Your memory usage beats 9.72 % of cpp submissions (12.6 MB)

--- 835.image-overlap.cpp
+++ 835.image-overlap.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class SolutionShiftAndCount {
 	/*
 	 * Shift m1 up-left/up-right, then count '1' in overlapped area.
 	 */
@@ -40,5 +40,71 @@ public:
 		return ans;
 	}
 };
-// @lc code=end
 
+	/*
+	 * e.g.  IMG1    IMG2
+	 *      1 1 0   0 0 0
+	 *      0 1 0   0 1 1
+	 *      0 1 0   0 0 1
+	 *
+	 *  coord1 = { (0,0) (0,1) (1,1) (2,1) }
+	 *  coord2 = { (1,1) (1,2) (2,2) }
+	 *
+	 *  try to align each coordination in coord1 to coord2, and track the number of
+	 *  overlapped areas.
+	 *
+	 *  for example, align (0,0) of IMG1 to (1,1) of IMG2,
+	 *                     (0,1)            (1,2)
+	 *                     (1,1)            (2,2)
+	 *  will get the most '1' in overlapped area
+	 */
+class SolutionMapping {
+	void dump(vector<pair<int, int>>& v) {
+		for (auto& i : v)
+			cout << "(" << i.first << ", " << i.second << ") ";
+		cout << "\n";
+	}
+
+	void dump2(map<pair<int, int>, int>& m) {
+		for (auto& i : m)
+			cout << "(" << i.first.first << "," << i.first.second << ") -> " << i.second << "\n";
+		cout << "\n";
+	}
+public:
+	int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+		vector<pair<int, int>> coord1, coord2;	// coordinate (x, y) where is '1' in img1 and img2
+		for (int i = 0; i < img1.size(); i++) {
+			for (int j = 0; j < img1.size(); j++) {
+				if (img1[i][j] == 1) coord1.push_back({ i, j });
+				if (img2[i][j] == 1) coord2.push_back({ i, j });
+			}
+		}
+	//	dump(coord1);
+	//	dump(coord2);
+
+		int ans = 0;
+		map<pair<int, int>, int> map1to2;		// { (x, y), count of 1 }
+		for (auto& p1 : coord1) {
+			for (auto& p2 : coord2) {
+				int x = p1.first - p2.first;	// x is the shift from coord1 to coord2 in x direction
+				int y = p1.second - p2.second;	// y is the shift from coord1 to coord2 in y direction
+				map1to2[{ x, y }]++;			// track the overlapped areas number in shift of (x, y)
+				ans = max(ans, map1to2[{ x, y }]);
+			}
+		}
+	//	dump2(map1to2);
+
+		return ans;
+	}
+};
+
+class Solution {
+	SolutionShiftAndCount shiftAndCount;
+	SolutionMapping mapping;
+public:
+	int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+	//	return shiftAndCount.largestOverlap(img1, img2);
+		return mapping.largestOverlap(img1, img2);
+	}
+};
+// @lc code=end
