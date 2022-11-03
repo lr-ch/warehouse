e59aec9c082da53668180a2da94ae83fb365b5d7
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-11-04 01:47:40 +0800

    * daily challenge (2022/11/03)   Accepted    * 120/120 cases passed (300 ms)    * Your runtime beats 97.69 % of cpp submissions    * Your memory usage beats 27.99 % of cpp submissions (168.1 MB)

---
+++ 2131.longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=2131 lang=cpp
+ *
+ * [2131] Longest Palindrome by Concatenating Two Letter Words
+ */
+
+// @lc code=start
+class Solution {
+public:
+	int longestPalindrome(vector<string>& words) {
+		unordered_map<string, int> freq;	// (string, count of strings)
+		for (auto& w : words) freq[w]++;
+
+		vector<string> seen;
+		int ans = 0;
+		bool middle = false, isodd = false;
+		for (auto& [str, count] : freq) {
+			string revstr(str.rbegin(), str.rend());
+
+			if (revstr != str) {
+				// e.g. "ab" -> "ba"
+				if (freq.count(revstr) &&
+					find(seen.begin(), seen.end(), str) == seen.end() &&
+					find(seen.begin(), seen.end(), revstr) == seen.end()) {
+					ans += min(freq[revstr], count) * 4;	// count == freq[str]
+															// a pair of "ab""ba" -> 4bytes
+
+					seen.push_back(revstr);
+					seen.push_back(str);
+				}
+			} else {
+				// e.g. "aa" -> "aa"
+				isodd = count % 2;
+				int divided = count / 2;
+				if (!middle && isodd) {
+					ans += 2;
+					middle = true;
+				}
+				ans += divided * 4;
+			}
+		}
+		return ans;
+	}
+};
+// @lc code=end
+
