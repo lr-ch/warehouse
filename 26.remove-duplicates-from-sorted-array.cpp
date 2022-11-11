41a58ae1b72f0d64505ab1a5b1e703c883e7d999
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-11-11 23:48:19 +0800

    * daily challenge (2022/11/11)   Accepted    * 361/361 cases passed (17 ms)    * Your runtime beats 78.78 % of cpp submissions    * Your memory usage beats 92.95 % of cpp submissions (18.3 MB)

--- 26.remove-duplicates-from-sorted-array.cpp
+++ 26.remove-duplicates-from-sorted-array.cpp
@@ -8,18 +8,11 @@
 class Solution {
 public:
 	int removeDuplicates(vector<int>& nums) {
-		if (nums.size() == 0) return 0;
-
-		int i = 0, j = 0;
-		while (j < nums.size()) {
-			if (nums[i] != nums[j]) {
-				i++;
-				nums[i] = nums[j];
-			}
-			j++;
-		}
-		return i + 1;
+		int slow = 0;
+		for (int fast = slow + 1; fast < nums.size(); fast++)
+			if (nums[slow] != nums[fast])
+				nums[++slow] = nums[fast];
+		return slow + 1;
 	}
 };
 // @lc code=end
-
