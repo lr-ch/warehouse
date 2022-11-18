c99010b7a11536c69834f868ba8c866eaea6cb74
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-11-18 08:23:21 +0800

    * daily challenge (2022/11/18)   Accepted    * 1013/1013 cases passed (0 ms)    * Your runtime beats 100 % of cpp submissions    * Your memory usage beats 19.97 % of cpp submissions (6 MB)

---
+++ 263.ugly-number.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=263 lang=cpp
+ *
+ * [263] Ugly Number
+ */
+
+// @lc code=start
+class Solution {
+public:
+	bool isUgly(int n) {
+		if (n <= 0) return false;
+
+		for (int i : {2, 3, 5})
+			while (n % i == 0) n /= i;
+		return n == 1;
+	}
+};
+// @lc code=end
+
