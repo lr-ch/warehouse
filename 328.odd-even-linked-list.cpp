6556b4542e448b5ecfaeb40896f352fd94bd7633
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-12-06 21:52:05 +0800

    * daily challenge (2022/12/06)   Accepted    * 70/70 cases passed (25 ms)    * Your runtime beats 52.67 % of cpp submissions    * Your memory usage beats 29.12 % of cpp submissions (10.5 MB)

---
+++ 328.odd-even-linked-list.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=328 lang=cpp
+ *
+ * [328] Odd Even Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode* oddEvenList(ListNode* head) {
+		if (!head || !head->next) return head;
+
+		ListNode *odd = head, *even = head->next;
+		ListNode *oe = even;	// for connection at last
+
+		ListNode empty;
+		empty.next = head;
+
+		while (even && even->next) {
+			// split odd elements
+			odd->next = even->next;
+			odd = odd->next;
+			// split even elements
+			even->next = odd->next;
+			even = even->next;
+		}
+		odd->next = oe;
+
+		return empty.next;
+	}
+};
+// @lc code=end
