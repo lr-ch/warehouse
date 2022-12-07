b6a215c52157fc8942848257b77698c234c6e37b
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-12-07 08:22:54 +0800

    * daily challenge (2022/12/08)   - Add BFS solution     Accepted      * 41/41 cases passed (129 ms)      * Your runtime beats 96.75 % of cpp submissions      * Your memory usage beats 6.69 % of cpp submissions (66.9 MB)

--- 938.range-sum-of-bst.cpp
+++ 938.range-sum-of-bst.cpp
@@ -16,7 +16,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class SolutionDfs {
 public:
 	int rangeSumBST(TreeNode* root, int low, int high) {
 		if (!root) return 0;
@@ -24,6 +24,30 @@ public:
 		int nodeval = 0;
 		if (root->val >= low && root->val <= high) nodeval = root->val;
 		return nodeval + rangeSumBST(root->right, low, high) + rangeSumBST(root->left, low, high);
+	}
+};
+
+class Solution {
+public:
+	int rangeSumBST(TreeNode* root, int low, int high) {
+		if (!root) return 0;
+
+		queue<TreeNode*> q;
+		q.push(root);
+
+		int ans = 0;
+		while (!q.empty()) {
+			int qsize = q.size();
+			while (qsize--) {
+				TreeNode *node = q.front();
+				q.pop();
+				if (node->val >= low && node->val <= high)
+					ans += node->val;
+				if (node->right) q.push(node->right);
+				if (node->left) q.push(node->left);
+			}
+		}
+		return ans;
 	}
 };
 // @lc code=end
