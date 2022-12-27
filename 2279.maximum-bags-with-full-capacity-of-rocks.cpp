a30c25ab4cc83de22f3adc292bce7e2efa48e7d2
lr-ch <107822234+lr-ch@users.noreply.github.com>
2022-12-27 08:59:30 +0800

    * daily challenge (2022/12/27)   - Add a new solution     Accepted      - 79/79 cases passed (202 ms)      - Your runtime beats 89.01 % of cpp submissions      - Your memory usage beats 12.75 % of cpp submissions (89.7 MB)

--- 2279.maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279.maximum-bags-with-full-capacity-of-rocks.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution1 {
 public:
 	int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
 		vector<pair<int, int>> bags;
@@ -34,5 +34,48 @@ public:
 		return totalBags;
 	}
 };
-// @lc code=end
 
+class Solution2 {
+public:
+	int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+		vector<int> remains;
+
+		for (int i = 0; i < capacity.size(); i++)
+			remains.push_back(capacity[i] - rocks[i]);
+		sort(remains.begin(), remains.end());
+
+		int fullBag = 0;
+		for (int remain : remains) {
+			if (additionalRocks >= remain) {
+				additionalRocks -= remain;
+				fullBag++;
+			} else
+				break;
+		}
+		return fullBag;
+	}
+};
+
+class Solution {
+	/*
+	 * Accepted
+	 *  79/79 cases passed (249 ms)
+	 *  Your runtime beats 72.09 % of cpp submissions
+	 *  Your memory usage beats 9.45 % of cpp submissions (94 MB)
+	 */
+	Solution1 sol1;
+
+	/*
+	 * Accepted
+	 *  79/79 cases passed (202 ms)
+	 *  Your runtime beats 89.01 % of cpp submissions
+	 *  Your memory usage beats 12.75 % of cpp submissions (89.6 MB)
+	 */
+	Solution2 sol2;
+public:
+	int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+	//	return sol1.maximumBags(capacity, rocks, additionalRocks);
+		return sol2.maximumBags(capacity, rocks, additionalRocks);
+	}
+};
+// @lc code=end
