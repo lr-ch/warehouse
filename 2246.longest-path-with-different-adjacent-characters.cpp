69727bb5fd2f5c3ffc51e12b2dffc8030df0552e
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-01-13 23:36:00 +0800

    * daily challenge (2023/01/13)   Accepted    * 141/141 cases passed (403 ms)    * Your runtime beats 89.73 % of cpp submissions    * Your memory usage beats 74.57 % of cpp submissions (177.9 MB)

---
+++ 2246.longest-path-with-different-adjacent-characters.cpp
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=2246 lang=cpp
+ *
+ * [2246] Longest Path With Different Adjacent Characters
+ */
+
+// @lc code=start
+class Solution {
+	int dfs(vector<vector<int>>& t, string& s, int node) {
+		/*
+		 * every node has at least length 1, but only node has two or more children could have maxlen2 > 0
+		 * e.g.1                     e.g.2                    e.g.3
+		 *    a    => maxlen1 = 2,     a    => maxlen1 = 2,     a    => maxlen1 = 1,
+		 *   / \      maxlen2 = 2     /        maxlen2 = 0              maxlen2 = 0
+		 *  b   c                    b
+		 */
+		int maxlen1 = 1, maxlen2 = 0;
+		for (const int child : t[node]) {
+			int len = dfs(t, s, child);
+
+			len = (s[child] == s[node] ? 1 : len + 1);
+
+			if (len > maxlen1) {
+				maxlen2 = maxlen1;
+				maxlen1 = len;
+			} else if (len > maxlen2) {
+				maxlen2 = len;
+			}
+		}
+
+		// if maxlen2 > 0, the node (root) length is counted twice, so it shall decrease 1
+		// see e.g.1 above
+		ans = max(ans, maxlen1 + (maxlen2 == 0 ? 0 : maxlen2 - 1));
+
+		return max(maxlen1, maxlen2);
+	}
+
+	int ans;
+public:
+	int longestPath(vector<int>& parent, string s) {
+		vector<vector<int>> tree(parent.size());
+		for (int i = 1; i < parent.size(); i++)
+			tree[parent[i]].push_back(i);
+
+		dfs(tree, s, 0);
+
+		return ans;
+	}
+};
+// @lc code=end
