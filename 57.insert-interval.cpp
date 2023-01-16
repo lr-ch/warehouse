a6bc89fc4a4054fd774a055a6d3db50328973a9a
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-01-16 21:04:13 +0800

    * daily challenge (2023/01/16)   - Add graph comments   - Accepted      + 156/156 cases passed (16 ms)      + Your runtime beats 83.48 % of cpp submissions      + Your memory usage beats 99.52 % of cpp submissions (16.9 MB)

--- 57.insert-interval.cpp
+++ 57.insert-interval.cpp
@@ -12,13 +12,29 @@ public:
 		for (const auto& it : intervals) {
 			if (it[1] < newInterval[0]) {
 			// if current_intervals.end < new_intervals.start
+			// e.g.
+			//      -----+------+--------+-------+-----
+			//           ^      ^        ^       ^
+			//               cur.end  new.start
 				res.push_back(it);
 			} else if (it[0] > newInterval[1]) {
 			// if current_intervals.start > new_intervals.end
+			// e.g.
+			//      -----+------+---------+------+-----
+			//           ^      ^         ^      ^
+			//               new.end  curr.start
 				res.push_back(newInterval);
 				newInterval = it;
 			} else {
-			// if (current_intervals.end > new_intervals.start) && (current_intervals.start < new_intervals.end)
+			// if current_intervals.end > new_intervals.start && current_intervals.start < new_intervals.end
+			// e.g.
+			//      -----+------+---------+------+-----
+			//           ^      ^         ^      ^
+			//              curr.start new.end
+			// or
+			//      -----+------+-----------+-----+----
+			//           ^      ^           ^     ^
+			//               new.start  curr.end
 				newInterval = { min(it[0], newInterval[0]), max(it[1], newInterval[1]) };
 			}
 		}
