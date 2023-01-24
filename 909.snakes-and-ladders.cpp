95377ea3658baef1a1347bee3da50cbaf347b856
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-01-25 00:37:48 +0800

    * daily challenge (2022/01/24)   - Fix buggy label_to_coord(), replaced with a static array   - Expect better performance yet not happened... :(   - Accepted      * 213/213 cases passed (194 ms)      * Your runtime beats 7.08 % of cpp submissions      * Your memory usage beats 10.09 % of cpp submissions (22.4 MB)

--- 909.snakes-and-ladders.cpp
+++ 909.snakes-and-ladders.cpp
@@ -6,24 +6,30 @@
 
 // @lc code=start
 class Solution {
-	pair<int, int> label_to_coord(int n) {
-		int row = N - n / N - 1;
-		int col;
+	void verifyTable(int n) {
+		for (int i = 0; i < n * n; i++) {
+			cout << "(" << label_to_coord[i].first << ", " << label_to_coord[i].second << ") ";
+			if ((i + 1) % n == 0) cout << endl;
+		}
+	}
 
-		if (N % 2)
-			col = row % 2 ? N - (n % N) - 1 : n % N;
-		else
-			col = row % 2 ? n % N : N - (n % N) - 1;
-
-		return { row, col };
+	void genCoordTable(int N) {
+		for (int row = N - 1, i = 0, reverse = 0; i < N; i++, row--) {
+			for (int j = 0, col = 0; j < N; j++, col++)
+				label_to_coord[i * N + j] = reverse ? pair<int, int>(row, N - 1 - col) : pair<int, int>(row, col);
+			reverse = !reverse;
+		}
+	//	verifyTable(N);
 	}
 
 	int N, goal;
+	array<pair<int, int>, 20 * 20> label_to_coord;
 public:
 	int snakesAndLadders(vector<vector<int>>& board) {
 		N = board.size();
 		goal = N * N - 1;
 
+		genCoordTable(N);
 		vector<bool> visited(N * N, false);
 		queue<int> square;
 		square.push(0);		// start from board[N - 1][0]
@@ -39,7 +45,7 @@ public:
 
 				// check coord and decide next step
 				for (int next = curr + 1; next <= min(curr + 6, goal); next++) {
-					auto coord = label_to_coord(next);
+					auto coord = label_to_coord[next];
 
 					// NOTE!! Our board is 0-indiced, the board's label is 1-indiced
 					int snake_ladder_next = board[coord.first][coord.second];
