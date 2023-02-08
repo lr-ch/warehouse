08a2ca253722886604917e8aabb05fd6be589e40
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-02-08 22:10:37 +0800

    * daily challenge (2022/02/08)   - Add greedy solution   - Accepted      - 109/109 cases passed (12 ms)      - Your runtime beats 92.2 % of cpp submissions      - Your memory usage beats 56.17 % of cpp submissions (16.6 MB)

--- 45.jump-game-ii.cpp
+++ 45.jump-game-ii.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class SolutionDFS {
 	int dp(vector<int>& n, int pos) {
 		if (pos >= n.size() - 1) return 0;	// reach the end
 
@@ -26,5 +26,49 @@ public:
 		return dp(nums, 0);
 	}
 };
-// @lc code=end
 
+class SolutionGreedy {
+public:
+	int jump(vector<int>& nums) {
+		if (nums.size() == 1) return 0;
+
+		int maxDistance = nums[0], steps = nums[0], jumps = 1;
+		for (int i = 1; i < nums.size(); i++) {
+			if (i == nums.size() - 1) return jumps;
+
+			maxDistance = max(maxDistance, i + nums[i]);
+			steps--;
+			if (!steps) {
+				jumps++;
+				steps = maxDistance - i;
+			}
+		}
+
+		return -1;
+	}
+};
+
+class Solution {
+	/*
+	 * Accepted
+	 *  - 109/109 cases passed (519 ms)
+	 *  - Your runtime beats 20.59 % of cpp submissions
+	 *  - Your memory usage beats 17.45 % of cpp submissions (17.6 MB)
+	 */
+	SolutionDFS dfs;
+
+	/*
+	 * Accepted
+	 *  - 109/109 cases passed (12 ms)
+	 *  - Your runtime beats 92.2 % of cpp submissions
+	 *  - Your memory usage beats 56.17 % of cpp submissions (16.6 MB)
+	 */
+	SolutionGreedy greedy;
+
+public:
+	int jump(vector<int>& nums) {
+	//	return dfs.jump(nums);
+		return greedy.jump(nums);
+	}
+};
+// @lc code=end
