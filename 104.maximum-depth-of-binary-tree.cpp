2e13588aac221b5705891ea84887d60711d8b450
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-02-16 08:34:49 +0800

    * daily challenge (2023/02/16)   - Add BFS solution   - Accepted      - 39/39 cases passed (7 ms)      - Your runtime beats 83.96 % of cpp submissions      - Your memory usage beats 19.79 % of cpp submissions (19.1 MB)

--- 104.maximum-depth-of-binary-tree.cpp
+++ 104.maximum-depth-of-binary-tree.cpp
@@ -16,7 +16,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class SolutionDFS {
 public:
 	int maxDepth(TreeNode* root) {
 		if (!root) return 0;
@@ -25,5 +25,50 @@ public:
 		return max(left, right);
 	}
 };
-// @lc code=end
 
+class SolutionBFS {
+public:
+	int maxDepth(TreeNode* root) {
+		if (!root) return 0;
+
+		queue<TreeNode *> q;
+		q.push(root);
+
+		int max = 0;
+		while (!q.empty()) {
+			int qsize = q.size();
+			max++;
+			while (qsize--) {
+				auto node = q.front();
+				q.pop();
+				if (node->left) q.push(node->left);
+				if (node->right) q.push(node->right);
+			}
+		}
+		return max;
+	}
+};
+
+class Solution {
+	/*
+	 * Accepted
+	 *  * 39/39 cases passed (7 ms)
+	 *  * Your runtime beats 83.96 % of cpp submissions
+	 *  * Your memory usage beats 99.7 % of cpp submissions (18.7 MB)
+	 */
+	SolutionDFS dfs;
+
+	/*
+	 * Accepted
+	 *  * 39/39 cases passed (7 ms)
+	 *  * Your runtime beats 83.96 % of cpp submissions
+	 *  * Your memory usage beats 19.79 % of cpp submissions (19.1 MB)
+	 */
+	SolutionBFS bfs;
+public:
+	int maxDepth(TreeNode* root) {
+	//	return dfs.maxDepth(root);
+		return bfs.maxDepth(root);
+	}
+};
+// @lc code=end
