4e642e674b8a5fa225e9d723307139924c8fd3fb
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-02-27 01:18:21 +0800

    * daily challenge (2023/02/26)   * Add a bottom-top solution   * Accepted      - 1146/1146 cases passed (62 ms)      - Your runtime beats 19.35 % of cpp submissions      - Your memory usage beats 79.78 % of cpp submissions (7.3 MB)

--- 72.edit-distance.cpp
+++ 72.edit-distance.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class SolutionTopBottom {
 	int dfs(string& w1, int m, string& w2, int n) {
 		if (m < 0) return n + 1;
 		if (n < 0) return m + 1;
@@ -29,9 +29,59 @@ public:
 	int minDistance(string word1, string word2) {
 		M = word1.length();
 		N = word2.length();
-		dp.resize(M + 1, vector<int>(N + 1, -1));
+		dp.resize(M, vector<int>(N, -1));
 
 		return dfs(word1, M - 1, word2, N - 1);
+	}
+};
+
+class SolutionBottomTop {
+	array<array<int, 501>, 501> dp;
+public:
+	int minDistance(string word1, string word2) {
+		int M = word1.length(), N = word2.length();
+
+		// base case
+		for (int i = 0; i <= M; i++) dp[i][0] = i;
+		for (int j = 0; j <= N; j++) dp[0][j] = j;
+
+		for (int x = 1; x <= M; x++) {
+			for (int y = 1; y <= N; y++) {
+				if (word1[x - 1] == word2[y - 1]) {
+					dp[x][y] = dp[x - 1][y - 1];	// do nothing
+				} else {
+					dp[x][y] = min({
+						dp[x][y - 1] + 1,			// insert
+						dp[x - 1][y - 1] + 1,		// modify
+						dp[x - 1][y] + 1			// delete
+					});
+				}
+			}
+		}
+		return dp[M][N];
+	}
+};
+
+class Solution {
+	/*
+	 * Accepted
+	 *  1146/1146 cases passed (6 ms)
+	 *  Your runtime beats 93.28 % of cpp submissions
+	 *  Your memory usage beats 63.2 % of cpp submissions (9 MB)
+	 */
+	SolutionTopBottom top;
+
+	/*
+	 * Accepted
+	 *  1146/1146 cases passed (23 ms)
+	 *  Your runtime beats 32.97 % of cpp submissions
+	 *  Your memory usage beats 72.1 % of cpp submissions (8.9 MB)
+	 */
+	SolutionBottomTop bottom;
+public:
+	int minDistance(string word1, string word2) {
+	//	return top.minDistance(word1, word2);
+		return bottom.minDistance(word1, word2);
 	}
 };
 // @lc code=end
