7ea37576678b603cfa1e6d0a1cdc1290077f48fe
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-03-06 23:45:45 +0800

    * daily challenge (2023/03/06)   - Improve algorithm of counting missed numbers   - Accepted      - 84/84 cases passed (0 ms)      - Your runtime beats 100 % of cpp submissions      - Your memory usage beats 38.05 % of cpp submissions (9.6 MB)

--- 1539.kth-missing-positive-number.cpp
+++ 1539.kth-missing-positive-number.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution1 {
 public:
 	int findKthPositive(vector<int>& arr, int k) {
 		vector<int> missed;
@@ -21,6 +21,50 @@ public:
 			count++;
 		}
 		return *arr.rbegin() + k - missed.size();
+	}
+};
+
+class Solution2 {
+public:
+	int findKthPositive(vector<int>& arr, int k) {
+		int count = 1, missed = 0;
+		for (int i = 0; i < arr.size(); i++, count++) {
+			if (count != arr[i]) {
+				// count missing numbers
+				missed += arr[i] - count;
+
+				// collected enough k?
+				if (missed >= k)
+					return arr[i] - (missed - k) - 1;
+
+				count = arr[i];
+			}
+		}
+		return *arr.rbegin() + k - missed;
+	}
+};
+
+class Solution {
+	/*
+	 * Accepted
+	 *  84/84 cases passed (3 ms)
+	 *  Your runtime beats 88.86 % of cpp submissions
+	 *  Your memory usage beats 20.52 % of cpp submissions (9.8 MB)
+	 */
+	Solution1 sol1;
+
+	/*
+	 * Accepted
+	 *  84/84 cases passed (0 ms)
+	 *  Your runtime beats 100 % of cpp submissions
+	 *  Your memory usage beats 38.05 % of cpp submissions (9.6 MB)
+	 */
+	Solution2 sol2;
+
+public:
+	int findKthPositive(vector<int>& arr, int k) {
+	//	return sol1.findKthPositive(arr, k);
+		return sol2.findKthPositive(arr, k);
 	}
 };
 // @lc code=end
