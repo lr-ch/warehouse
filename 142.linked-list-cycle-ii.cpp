6d2a8608b5df80f3101b004f4ee14852e513f2b5
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-03-09 22:32:26 +0800

    * daily challenge (2023/03/09)   - Add a space O(1) solution   - Accepted      - 17/17 cases passed (8 ms)      - Your runtime beats 76.5 % of cpp submissions      - Your memory usage beats 84.91 % of cpp submissions (7.5 MB)

--- 142.linked-list-cycle-ii.cpp
+++ 142.linked-list-cycle-ii.cpp
@@ -13,7 +13,7 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
+class SolutionSpaceOn {
 	unordered_set<ListNode *> seen;
 public:
 	ListNode *detectCycle(ListNode *head) {
@@ -25,6 +25,55 @@ public:
 			head = head->next;
 		}
 		return nullptr;
+	}
+};
+
+class SolutionSpaceO1 {
+public:
+	ListNode *detectCycle(ListNode *head) {
+		ListNode *slow = head, *fast = head;
+
+		while (fast && fast->next) {
+			slow = slow->next;
+			fast = fast->next->next;
+
+			// it only means there is a cycle, not sure this is the cross point
+			if (fast == slow) break;
+		}
+
+		// there is no cycle
+		if (!fast || !fast->next) return nullptr;
+
+		// reset slow, sync steps until meets fast
+		slow = head;
+		while (slow != fast) {
+			fast = fast->next;
+			slow = slow->next;
+		}
+		return slow;	// or fast
+	}
+};
+
+class Solution {
+	/*
+	 * Accepted
+	 *  17/17 cases passed (16 ms)
+	 *  Your runtime beats 23.25 % of cpp submissions
+	 *  Your memory usage beats 19.71 % of cpp submissions (9.5 MB)
+	 */
+	SolutionSpaceOn on;
+
+	/*
+	 * Accepted
+	 *  17/17 cases passed (8 ms)
+	 *  Your runtime beats 76.5 % of cpp submissions
+	 *  Your memory usage beats 84.91 % of cpp submissions (7.5 MB)
+	 */
+	SolutionSpaceO1 o1;
+public:
+	ListNode *detectCycle(ListNode *head) {
+		return on.detectCycle(head);
+	//	return o1.detectCycle(head);
 	}
 };
 // @lc code=end
