fc392918fd226ee479e2a170f70652da0df4eca4
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-04-01 10:20:52 +0800

    * daily challenge (2023/04/01)   Accepted    * 47/47 cases passed (49 ms)    * Your runtime beats 9.98 % of cpp submissions    * Your memory usage beats 18.34 % of cpp submissions (27.6 MB)

---
+++ 704.binary-search.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=704 lang=cpp
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+class Solution {
+public:
+	int search(vector<int>& nums, int target) {
+		int left = 0, right = nums.size() - 1;
+
+		while (left <= right) {
+			int mid = (left + right) / 2;
+
+			if (nums[mid] == target)
+				return mid;
+			else if (nums[mid] < target)
+				left = mid + 1;
+			else
+				right = mid - 1;
+		}
+		return -1;
+	}
+};
+// @lc code=end
