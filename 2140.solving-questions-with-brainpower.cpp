3fe6148910e5ad7776a151dcfcc6f2fc4095ad6c
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-05-12 22:11:16 +0800

    * daily challenge (2023/05/12)   - Add an iteration solution     - 54/54 cases passed (404 ms)     - Your runtime beats 20.22 % of cpp submissions     - Your memory usage beats 93.26 % of cpp submissions (115.2 MB)

--- 2140.solving-questions-with-brainpower.cpp
+++ 2140.solving-questions-with-brainpower.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class SolutionRecur {
 	long long dfs(vector<vector<int>>& q, int i) {
 		if (i >= q.size()) return 0;
 
@@ -19,6 +19,49 @@ public:
 	long long mostPoints(vector<vector<int>>& questions) {
 		memo.resize(questions.size(), -1);
 		return dfs(questions, 0);
+	}
+};
+
+class SolutionIter {
+public:
+	long long mostPoints(vector<vector<int>>& questions) {
+		int N = questions.size();
+		vector<long long> dp(N, 0);
+		dp[N - 1] = questions.back()[0];
+
+		for (int i = N - 2; i >= 0; i--) {
+			// Add up points of the questions after skipped ones
+			if (i + questions[i][1] + 1 < N)
+				dp[i] = dp[i + questions[i][1] + 1];
+
+			// select the maximun points from select/unselect
+			dp[i] = max(dp[i] + questions[i][0], dp[i + 1]);
+		}
+
+		return dp[0];
+	}
+};
+
+class Solution {
+	/*
+	 * Accepted
+	 *  - 54/54 cases passed (397 ms)
+	 *  - Your runtime beats 28.09 % of cpp submissions
+	 *  - Your memory usage beats 50.34 % of cpp submissions (121.6 MB)
+	 */
+	SolutionRecur recur;
+
+	/*
+	 * Accepted
+	 *  - 54/54 cases passed (404 ms)
+	 *  - Your runtime beats 20.22 % of cpp submissions
+	 *  - Your memory usage beats 93.26 % of cpp submissions (115.2 MB)
+	 */
+	SolutionIter iter;
+public:
+	long long mostPoints(vector<vector<int>>& questions) {
+	//	return recur.mostPoints(questions);
+		return iter.mostPoints(questions);
 	}
 };
 // @lc code=end
