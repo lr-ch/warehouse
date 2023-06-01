fb897b69711fd02c884798a3a3717ae699dfd2a7
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-06-01 21:02:19 +0800

    * daily challenge (2023/06/01)   - Optimize space effeciency   - Accepted      - 89/89 cases passed (59 ms)      - Your runtime beats 95.06 % of cpp submissions      - Your memory usage beats 85.79 % of cpp submissions (19.3 MB)

--- 1091.shortest-path-in-binary-matrix.cpp
+++ 1091.shortest-path-in-binary-matrix.cpp
@@ -10,29 +10,28 @@ class Solution {
 		return r >= 0 && r < n && c >= 0 && c < n && g[r][c] == 0;
 	}
 
-	array<pair<int, int>, 8> dirs = { pair<int, int>(1,  0), { 0,  1}, {-1,  0}, { 0, -1},
-													{1,  1}, { 1, -1}, {-1, -1}, {-1,  1} };
 	int n;
+	const vector<pair<int, int>> dirs = { {1,  0}, { 0,  1}, {-1,  0}, { 0, -1},
+										  {1,  1}, { 1, -1}, {-1, -1}, {-1,  1} };
 public:
 	int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
 		n = grid.size();
 
-		queue<array<int, 3>> q;
+		queue<pair<int, int>> q;
 		if (grid[0][0] == 0) {
-			grid[0][0] = 2;		// marked as visited
-			q.push({ 0, 0, 1 });
+			grid[0][0] = 1;
+			q.push({ 0, 0 });
 		}
 
 		while (!q.empty()) {
-			auto pos = q.front(); q.pop();
-			int row = pos[0], col = pos[1], steps = pos[2];
+			auto [row, col] = q.front(); q.pop();
 
-			if (row == n - 1 && col == n - 1) return steps;
+			if (row == n - 1 && col == n - 1) return grid[row][col];
 
 			for (const auto& [r, c] : dirs) {
 				if (isValidCoord(row + r, col + c, grid)) {
-					grid[row + r][col + c] = 2;		// marked as visited
-					q.push({ row + r, col + c, steps + 1 });
+					grid[row + r][col + c] = grid[row][col] + 1;
+					q.push({ row + r, col + c });
 				}
 			}
 		}
