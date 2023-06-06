b7d0be6a9572fb29b76e92f63a2382d7ef9ffc20
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-06-06 08:47:39 +0800

    * daily challenge (2023/06/06)   Accepted    * 109/109 cases passed (9 ms)    * Your runtime beats 22.98 % of cpp submissions    * Your memory usage beats 66.22 % of cpp submissions (9.1 MB)

---
+++ 1502.can-make-arithmetic-progression-from-sequence.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=1502 lang=cpp
+ *
+ * [1502] Can Make Arithmetic Progression From Sequence
+ */
+
+// @lc code=start
+class Solution {
+public:
+	bool canMakeArithmeticProgression(vector<int>& arr) {
+		sort(arr.begin(), arr.end());
+
+		int diff = arr[1] - arr[0];
+		for (int i = 1; i < arr.size() - 1; i++)
+			if (arr[i + 1] - arr[i] != diff)
+				return false;
+		return true;
+	}
+};
+// @lc code=end
