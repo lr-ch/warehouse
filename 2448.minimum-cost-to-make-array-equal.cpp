e34aafdae6c4c74ef898b4f29b6759cbd89d76d5
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-06-22 00:10:10 +0800

    * daily challenge (2023/06/21)   - Add a new solution, by finding the middle number directly   - Accepted      - 48/48 cases passed (87 ms)      - Your runtime beats 94.89 % of cpp submissions      - Your memory usage beats 36.86 % of cpp submissions (41.7 MB)

--- 2448.minimum-cost-to-make-array-equal.cpp
+++ 2448.minimum-cost-to-make-array-equal.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class SolutionConvex {
 	long long calCost(int base) {
 		long long res = 0;
 		for (const auto& [val, cost] : valCost)
@@ -34,6 +34,55 @@ public:
 		}
 
 		return ans;
+	}
+};
+
+class SolutionMid {
+public:
+	long long minCost(vector<int>& nums, vector<int>& cost) {
+		vector<pair<int, int>> valCost;
+		for (int i = 0; i < nums.size(); i++)
+			valCost.push_back({ nums[i], cost[i] });
+		sort(valCost.begin(), valCost.end());
+
+		long long totalCost = accumulate(cost.begin(), cost.end(), 0LL), accu = 0;
+		int mid = 0;
+		for (int i = 0; i < valCost.size(); i++) {
+			accu += valCost[i].second;
+			if (accu >= 1.0 * totalCost / 2) {
+				mid = i;
+				break;
+			}
+		}
+
+		long long ans =  0;
+		for (int i = 0; i < valCost.size(); i++)
+			ans += (long long)abs(valCost[i].first - valCost[mid].first) * valCost[i].second;
+		return ans;
+	}
+};
+
+class Solution {
+
+	/*
+	 * Accepted
+	 *  - 48/48 cases passed (109 ms)
+	 *  - Your runtime beats 59.49 % of cpp submissions
+	 *  - Your memory usage beats 42.34 % of cpp submissions (41.6 MB)
+	 */
+	SolutionConvex convex;
+
+	/*
+	 * Accepted
+	 *  - 48/48 cases passed (89 ms)
+	 *  - Your runtime beats 93.8 % of cpp submissions
+	 *  - Your memory usage beats 42.34 % of cpp submissions (41.6 MB)
+	 */
+	SolutionMid mid;
+public:
+	long long minCost(vector<int>& nums, vector<int>& cost) {
+	//	return convex.minCost(nums, cost);
+		return mid.minCost(nums, cost);
 	}
 };
 // @lc code=end
