bb9dbe1f9888d178412ce59f21bf85bb55b533af
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-07-02 22:00:38 +0800

    * daily challenge (2023/07/02)   - Add a recursive backtrack solution   - Accepted      - 117/117 cases passed (106 ms)      - Your runtime beats 69.61 % of cpp submissions      - Your memory usage beats 92.16 % of cpp submissions (8.7 MB)

--- 1601.maximum-number-of-achievable-transfer-requests.cpp
+++ 1601.maximum-number-of-achievable-transfer-requests.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class SolutionTab {
 public:
 	int maximumRequests(int n, vector<vector<int>>& requests) {
 		vector<bitset<16>> cb(pow(2, requests.size()));
@@ -33,6 +33,56 @@ public:
 		}
 
 		return ans;
+	}
+};
+
+class SolutionRecur {
+	void bt(vector<vector<int>>& r, vector<int>& b, int applied, int i) {
+		if (i == r.size()) {
+			auto non_zero = find_if(b.begin(), b.end(), [](const int i) {return i != 0;});
+			if (non_zero == b.end())
+				ans = max(ans, applied);
+			return;
+		}
+
+		// do not apply this request
+		bt(r, b, applied, i + 1);
+
+		// apply this request
+		b[r[i][0]]--; b[r[i][1]]++;
+		bt(r, b, applied + 1, i + 1);
+		b[r[i][0]]++; b[r[i][1]]--;
+	}
+
+	int ans = 0;
+public:
+	int maximumRequests(int n, vector<vector<int>>& requests) {
+		vector<int> buildings(n, 0);
+		bt(requests, buildings, 0, 0);
+		return ans;
+	}
+};
+
+class Solution {
+	/*
+	 * Accepted
+	 *  - 117/117 cases passed (378 ms)
+	 *  - Your runtime beats 39.22 % of cpp submissions
+	 *  - Your memory usage beats 40.2 % of cpp submissions (24.8 MB)
+	 */
+	SolutionTab tab;
+
+	/*
+	 * Accepted
+	 *  - 117/117 cases passed (106 ms)
+	 *  - Your runtime beats 69.61 % of cpp submissions
+	 *  - Your memory usage beats 92.16 % of cpp submissions (8.7 MB)
+	 */
+	SolutionRecur recur;
+public:
+	int maximumRequests(int n, vector<vector<int>>& requests) {
+	//	return tab.maximumRequests(n, requests);
+		return recur.maximumRequests(n, requests);
 	}
 };
 // @lc code=end
