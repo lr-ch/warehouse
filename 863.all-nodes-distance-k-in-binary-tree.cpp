538de1a1c3977b772e80f106c0684c5c406a305f
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-07-11 19:49:56 +0800

    * daily challenge (2023/07/11)   Accepted    * 57/57 cases passed (7 ms)    * Your runtime beats 65.63 % of cpp submissions    * Your memory usage beats 5.02 % of cpp submissions (18.3 MB)

---
+++ 863.all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=863 lang=cpp
+ *
+ * [863] All Nodes Distance K in Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+	void buildAdjs(TreeNode* parent, TreeNode* current) {
+		if (parent && current) {
+			adjs[parent->val].insert(current->val);
+			adjs[current->val].insert(parent->val);
+		}
+
+		if (current && current->left)
+			buildAdjs(current, current->left);
+
+		if (current && current->right)
+			buildAdjs(current, current->right);
+	}
+
+	unordered_map<int, unordered_set<int>> adjs;
+public:
+	vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+		buildAdjs(nullptr, root);
+
+		queue<int> q;
+		q.push(target->val);
+		vector<bool> v(500, false);
+		v[target->val] = true;
+
+		vector<int> ans;
+		while (!q.empty() && k >= 0) {
+			if (k == 0) {
+				while (!q.empty()) {
+					ans.push_back(q.front());
+					q.pop();
+				}
+				return ans;
+			}
+
+			int qsize = q.size();
+			while (qsize--) {
+				int node = q.front(); q.pop();
+				v[node] = true;
+				for (const auto& next : adjs[node]) {
+					if (!v[next]) q.push(next);
+				}
+			}
+
+			k--;
+		}
+
+		return ans;
+	}
+};
+// @lc code=end
