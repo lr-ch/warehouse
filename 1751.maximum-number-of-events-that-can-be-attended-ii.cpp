7c0aafb328d00f9c9aa08afa5cbf36727f4f7881
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-07-15 18:42:35 +0800

    * daily challenge (2023/07/15)   - Replace binary search by simply comparison of current start time and last end time   - Accepted      - 67/67 cases passed (258 ms)      - Your runtime beats 99.37 % of cpp submissions      - Your memory usage beats 83.1 % of cpp submissions (67.1 MB)

--- 1751.maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751.maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -29,7 +29,7 @@ public:
 	}
 };
 
-class Solution {
+class SolutionBinSearchDfs {
 	int dfs(vector<vector<int>>& e, int k, int cur) {
 		if (k == 0 || cur == e.size()) return 0;
 
@@ -51,6 +51,52 @@ public:
 
 		memo.resize(k + 1, vector<int>(events.size(), -1));
 		return dfs(events, k, 0);
+	}
+};
+
+class SolutionCompareEndTime {
+	int dfs(vector<vector<int>>& e, int k, int cur, int end) {
+		if (k == 0 || cur == e.size()) return 0;
+
+		// If current start time is less than previous end time then skip this event
+		if (e[cur][0] <= end) return dfs(e, k, cur + 1, end);
+
+		if (memo[k][cur] != -1) return memo[k][cur];
+
+		return memo[k][cur] = max(
+								e[cur][2] + dfs(e, k - 1, cur + 1, e[cur][1]),	// take this event
+								dfs(e, k, cur + 1, end));						// skip this event
+	}
+
+	vector<vector<int>> memo;
+public:
+	int maxValue(vector<vector<int>>& events, int k) {
+		sort(events.begin(), events.end());
+
+		memo.resize(k + 1, vector<int>(events.size(), -1));
+		return dfs(events, k, 0, 0);
+	}
+};
+
+class Solution {
+	/*
+	 * Accepted
+	 *  - 67/67 cases passed (346 ms)
+	 *  - Your runtime beats 55.45 % of cpp submissions
+	 *  - Your memory usage beats 8.05 % of cpp submissions (106.9 MB)
+	 */
+	SolutionBinSearchDfs bs_dfs;
+	/*
+	 * Accepted
+	 *  - 67/67 cases passed (272 ms)
+	 *  - Your runtime beats 98.26 % of cpp submissions
+	 *  - Your memory usage beats 83.1 % of cpp submissions (67.2 MB)
+	 */
+	SolutionCompareEndTime comp_end;
+public:
+	int maxValue(vector<vector<int>>& events, int k) {
+	//	return bs_dfs.maxValue(events, k);
+		return comp_end.maxValue(events, k);
 	}
 };
 // @lc code=end
