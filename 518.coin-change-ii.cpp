5c25a80c0aff3376479c5b6f146c9bb940cf96ce
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-08-11 09:42:38 +0800

    * daily challenge (2023/08/11)   * Add a bottom-up solution   * Accepted      - 28/28 cases passed (3 ms)      - Your runtime beats 99.13 % of cpp submissions      - Your memory usage beats 86.45 % of cpp submissions (7.2 MB)

--- 518.coin-change-ii.cpp
+++ 518.coin-change-ii.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class SolutionTopDown {
 	int dp(int curr, const vector<int>& coins, int index) {
 		if (curr == 0) return 1;
 		if (curr < 0 || index >= coins.size()) return 0;
@@ -26,6 +26,43 @@ public:
 	int change(int amount, vector<int>& coins) {
 		memo.resize(amount + 1, vector<int>(coins.size(), -1));
 		return dp(amount, coins, 0);
+	}
+};
+
+class SolutionBottomUp {
+public:
+	int change(int amount, vector<int>& coins) {
+		vector<int> dp(amount + 1);
+		dp[0] = 1;
+
+		for (int i = 0; i < coins.size(); i++)
+			for (int j = coins[i]; j <= amount; j++)
+				dp[j] += dp[j - coins[i]];
+
+		return dp[amount];
+	}
+};
+
+class Solution {
+	/*
+	 * Accepted
+	 *  - 28/28 cases passed (39 ms)
+	 *  - Your runtime beats 43.11 % of cpp submissions
+	 *  - Your memory usage beats 11.46 % of cpp submissions (21.7 MB)
+	 */
+	SolutionTopDown td;
+
+	/*
+	 * Accepted
+	 *  - 28/28 cases passed (3 ms)
+	 *  - Your runtime beats 99.13 % of cpp submissions
+	 *  - Your memory usage beats 86.45 % of cpp submissions (7.2 MB)
+	 */
+	SolutionBottomUp bu;
+public:
+	int change(int amount, vector<int>& coins) {
+	//	return td.change(amount, coins);
+		return bu.change(amount, coins);
 	}
 };
 // @lc code=end
