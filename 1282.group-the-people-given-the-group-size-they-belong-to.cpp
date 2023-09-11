f831d56abe4de65a7aa31b1bcbeafddcf6085835
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-09-11 22:17:42 +0800

    * daily challenge (2023/09/11)   - Simplify process   - Accepted      - 103/103 cases passed (8 ms)      - Your runtime beats 78.38 % of cpp submissions      - Your memory usage beats 80.59 % of cpp submissions (12.7 MB)

--- 1282.group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282.group-the-people-given-the-group-size-they-belong-to.cpp
@@ -6,31 +6,18 @@
 
 // @lc code=start
 class Solution {
-	void split_group(int size, vector<int>& members) {
-		vector<int> tmp;
-		for (int i = 1; i <= members.size(); i++) {
-			if (i % size == 0) {
-				tmp.push_back(members[i - 1]);
-				ret.push_back(tmp);
-				tmp.clear();
-			} else
-				tmp.push_back(members[i - 1]);
-		}
-	}
-
-	vector<vector<int>> ret;
 public:
 	vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
 		unordered_map<int, vector<int>> groups;		// (group size, members)
+		vector<vector<int>> ret;
 
-		for (int i = 0; i < groupSizes.size(); i++)
+		for (int i = 0; i < groupSizes.size(); i++) {
 			groups[groupSizes[i]].push_back(i);
 
-		for (auto &[size, members] : groups) {
-			if (members.size() > size)
-				split_group(size, members);
-			else
-				ret.push_back(members);
+			if (groups[groupSizes[i]].size() == groupSizes[i]) {
+				ret.push_back(groups[groupSizes[i]]);
+				groups[groupSizes[i]].clear();
+			}
 		}
 
 		return ret;
