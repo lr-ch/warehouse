0120b43be0035a4dd3bd79830a9d8e3664c0606a
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-09-22 22:20:31 +0800

    * daily challenge (2023/09/22)   - Add a optimized solution for follow up cases : fixed t and multiple s   - Accepted      - 19/19 cases passed (0 ms)      - Your runtime beats 100 % of cpp submissions      - Your memory usage beats 7.06 % of cpp submissions (7.1 MB)

--- 392.is-subsequence.cpp
+++ 392.is-subsequence.cpp
@@ -5,9 +5,9 @@
  */
 
 // @lc code=start
-class Solution {
+class SolutionTwoPtr {
 public:
-	bool isSubsequence(string s, string t) {
+	bool isSubsequence(string& s, string& t) {
 		auto ps = s.begin(), pt = t.begin();
 
 		while (pt != t.end() && ps != s.end()) {
@@ -16,6 +16,47 @@ public:
 		}
 
 		return ps == s.end();
+	}
+};
+
+class SolutionMemo {
+public:
+	bool isSubsequence(string& s, string& t) {
+		unordered_map<char, vector<int>> char_index;		// (char, indices of char resides)
+		for (int i = 0; i < t.size(); i++)
+			char_index[t[i]].push_back(i);
+
+		int i = -1;		// init for the first char
+		for (const char ch : s) {
+			auto iter = upper_bound(char_index[ch].begin(), char_index[ch].end(), i);
+
+			if (iter == char_index[ch].end()) return false;	// if no more char resides after i
+			i = *iter;
+		}
+		return true;
+	}
+};
+
+class Solution {
+	/*
+	 * Accepted
+	 *  - 19/19 cases passed (0 ms)
+	 *  - Your runtime beats 100 % of cpp submissions
+	 *  - Your memory usage beats 17.6 % of cpp submissions (6.6 MB)
+	 */
+	SolutionTwoPtr twoPtr;
+
+	/*
+	 * Accepted
+	 *  - 19/19 cases passed (0 ms)
+	 *  - Your runtime beats 100 % of cpp submissions
+	 *  - Your memory usage beats 7.06 % of cpp submissions (7.1 MB)
+	 */
+	SolutionMemo memo;
+public:
+	bool isSubsequence(string& s, string& t) {
+	//	return twoPtr.isSubsequence(s, t);
+		return memo.isSubsequence(s, t);
 	}
 };
 // @lc code=end
