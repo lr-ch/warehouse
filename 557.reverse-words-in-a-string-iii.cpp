8c910dbbe7f9d0d07b44038f4ab781f2b51acf48
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-10-01 11:22:15 +0800

    * daily challenge (2023/10/01)    * Add a two-pointers solution    * Accepted       - 29/29 cases passed (11 ms)       - Your runtime beats 85.17 % of cpp submissions       - Your memory usage beats 48.7 % of cpp submissions (10.5 MB)

--- 557.reverse-words-in-a-string-iii.cpp
+++ 557.reverse-words-in-a-string-iii.cpp
@@ -5,14 +5,14 @@
  */
 
 // @lc code=start
-class Solution {
+class SolutionTrivial {
 	void split(string& str, vector<string>& output) {
 		string ln;
 		stringstream ss(str);
 		while (getline(ss, ln, ' ')) output.push_back(ln);
 	}
 public:
-	string reverseWords(string s) {
+	string reverseWords(string& s) {
 		vector<string> splitted;
 		split(s, splitted);
 
@@ -21,8 +21,50 @@ public:
 			ans += string(ss.rbegin(), ss.rend()) + ' ';
 
 		// remove trailing space
-		return ans.substr(0, ans.length() - 1);
+		ans.pop_back();
+		return ans;
+	}
+};
+
+class SolutionTwoPtr {
+public:
+	string reverseWords(string& s) {
+		int is = -1;	// index of last space
+		for (int i = 0; i <= s.length(); i++) {
+			if (s[i] == ' ' || s.length() == i) {
+				int left = is + 1, right = i - 1;
+				while (left < right) {
+					swap(s[left], s[right]);
+					left++;
+					right--;
+				}
+				is = i;
+			}
+		}
+		return s;
+	}
+};
+
+class Solution {
+	/*
+	 * Accepted
+	 *  - 29/29 cases passed (26 ms)
+	 *  - Your runtime beats 14.16 % of cpp submissions
+	 *  - Your memory usage beats 12.78 % of cpp submissions (16.4 MB)
+	 */
+	SolutionTrivial trivial;
+
+	/*
+	 * Accepted
+	 *  - 29/29 cases passed (10 ms)
+	 *  - Your runtime beats 92.55 % of cpp submissions
+	 *  - Your memory usage beats 59.58 % of cpp submissions (10 MB)
+	 */
+	SolutionTwoPtr twoPtr;
+public:
+	string reverseWords(string s) {
+	//	return trivial.reverseWords(s);
+		return twoPtr.reverseWords(s);
 	}
 };
 // @lc code=end
-
