efcb52f967ced9c7a7345cf8c94bafbb8b2def64
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-10-16 23:17:27 +0800

    * daily challenge (2023/10/16)    - Add a tabulation solution    - Accepted       - 34/34 cases passed (3 ms)       - Your runtime beats 36.93 % of cpp submissions       - Your memory usage beats 25.89 % of cpp submissions (6.8 MB)

--- 119.pascals-triangle-ii.cpp
+++ 119.pascals-triangle-ii.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class SolutionRecur {
 	vector<int> genRow(vector<int>& curr, int rowIndex) {
 		if (0 == rowIndex) return curr;
 
@@ -19,6 +19,43 @@ public:
 	vector<int> getRow(int rowIndex) {
 		vector<int> init(1, 1);
 		return genRow(init, rowIndex);
+	}
+};
+
+class SolutionTab {
+public:
+	vector<int> getRow(int rowIndex) {
+		vector<int> curr(1, 1);
+		for (int n = 0; n < rowIndex; n++) {
+			vector<int> next(curr.size() + 1, 1);
+			for (int i = 1; i < curr.size(); i++)
+				next[i] = curr[i - 1] + curr[i];
+			curr = next;
+		}
+		return curr;
+	}
+};
+
+class Solution {
+	/*
+	 * Accepted
+	 *  - 34/34 cases passed (3 ms)
+	 *  - Your runtime beats 36.93 % of cpp submissions
+	 *  - Your memory usage beats 7.53 % of cpp submissions (7.2 MB)
+	 */
+	SolutionRecur recur;
+
+	/*
+	 * Accepted
+	 *  - 34/34 cases passed (3 ms)
+	 *  - Your runtime beats 36.93 % of cpp submissions
+	 *  - Your memory usage beats 11.01 % of cpp submissions (7.1 MB)
+	 */
+	SolutionTab tab;
+public:
+	vector<int> getRow(int rowIndex) {
+	//	return recur.getRow(rowIndex);
+		return tab.getRow(rowIndex);
 	}
 };
 // @lc code=end
