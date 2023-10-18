3c49564210ab5d6408544c77473fd9e5148e0d6d
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-10-18 23:41:27 +0800

    * daily challenge (2023/10/18)    - Add a DFS solution    - Accepted       - 42/42 cases passed (358 ms)       - Your runtime beats 44.06 % of cpp submissions       - Your memory usage beats 13.85 % of cpp submissions (156.9 MB)

--- 2050.parallel-courses-iii.cpp
+++ 2050.parallel-courses-iii.cpp
@@ -5,7 +5,7 @@
  */
 
 // @lc code=start
-class Solution {
+class SolutionBFS {
 public:
 	int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
 		unordered_map<int, vector<int>> adjs;	// (from, [to...])
@@ -34,6 +34,55 @@ public:
 		}
 
 		return *max_element(course_time.begin(), course_time.end());
+	}
+};
+
+class SolutionDFS {
+	int dfs(int curr, vector<int>& t) {
+		if (0 == adjs[curr].size()) return t[curr - 1];
+		if (-1 != course_time[curr]) return course_time[curr];
+
+		int res = 0;
+		for (auto& next : adjs[curr])
+			res = max(res, dfs(next, t));
+		return course_time[curr] = res + t[curr - 1];
+	}
+
+	unordered_map<int, vector<int>> adjs;
+	vector<int> course_time;
+public:
+	int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
+		course_time.assign(n + 1, -1);
+		for (auto& r : relations)
+			adjs[r[0]].push_back(r[1]);
+
+		int ans = 0;
+		for (int i = 1; i <= n; i++)
+			ans = max(ans, dfs(i, time));
+		return ans;
+	}
+};
+
+class Solution {
+	/*
+	 * Accepted
+	 *  - 42/42 cases passed (331 ms)
+	 *  - Your runtime beats 59.17 % of cpp submissions
+	 *  - Your memory usage beats 42.62 % of cpp submissions (144.6 MB)
+	 */
+	SolutionBFS bfs;
+
+	/*
+	 * Accepted
+	 *  - 42/42 cases passed (361 ms)
+	 *  - Your runtime beats 41.37 % of cpp submissions
+	 *  - Your memory usage beats 13.85 % of cpp submissions (157 MB)
+	 */
+	SolutionDFS dfs;
+public:
+	int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
+	//	return bfs.minimumTime(n, relations, time);
+		return dfs.minimumTime(n, relations, time);
 	}
 };
 // @lc code=end
