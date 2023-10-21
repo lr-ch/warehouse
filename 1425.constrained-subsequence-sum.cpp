fb625c619e553e0309bcdca9c810a4d4561bf9a1
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-10-21 16:59:10 +0800

    * daily challenge (2023/10/21)    Accepted     * 36/36 cases passed (215 ms)     * Your runtime beats 71.43 % of cpp submissions     * Your memory usage beats 97.76 % of cpp submissions (115.1 MB)

---
+++ 1425.constrained-subsequence-sum.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=1425 lang=cpp
+ *
+ * [1425] Constrained Subsequence Sum
+ */
+
+// @lc code=start
+class Solution {
+	template <typename T>
+	struct MonotonicQueue {
+		void push(T e) {
+			while (!q.empty() && e >= q.back()) q.pop_back();
+			q.push_back(e);
+		}
+
+		T max() { return q.front(); }
+
+		void pop() { q.pop_front(); }
+
+		size_t size() { return q.size(); }
+
+		bool empty() { return q.empty(); }
+
+	private:
+		deque<T> q;
+	};
+
+public:
+	int constrainedSubsetSum(vector<int>& nums, int k) {
+		MonotonicQueue<pair<int, int>> mq;	// (sum, index)
+
+		int ans = INT_MIN;
+		for (int i = 0; i < nums.size(); i++) {
+			// is the queue full?
+			if (i > k && mq.size() > 0 && mq.max().second == i - k - 1)
+				mq.pop();
+
+			int sum;
+			if (i == 0 || mq.size() == 0)
+				sum = nums[i];
+			else
+				sum = max(mq.max().first, 0) + nums[i];
+
+			mq.push({ sum, i });
+			ans = max(ans, sum);
+		}
+
+		return ans;
+	}
+};
+// @lc code=end
