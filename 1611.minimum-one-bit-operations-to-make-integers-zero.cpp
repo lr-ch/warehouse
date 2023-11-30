e6275d9d074845a3c9a09719df5ef799ee96b201
lr-ch <107822234+lr-ch@users.noreply.github.com>
2023-11-30 23:01:47 +0800

    * daily challenge (2023/11/30)   Accepted    * 308/308 cases passed (2 ms)    * Your runtime beats 46.92 % of cpp submissions    * Your memory usage beats 8.06 % of cpp submissions (7.3 MB)

---
+++ 1611.minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1611 lang=cpp
+ *
+ * [1611] Minimum One Bit Operations to Make Integers Zero
+ */
+
+// @lc code=start
+class Solution {
+	unordered_map<int, int> memo;
+public:
+	int minimumOneBitOperations(int n) {
+		if (n == 0) return 0;
+		if (n == 1) return 1;
+		if (memo.find(n) != memo.end()) return memo[n];
+
+		// find leading bit of n
+		string str = bitset<32>(n).to_string();
+		int leading = 0;
+		for (int i = 31; i >= 0; i--)
+			if (str[i] == '1') leading = 31 - i;
+
+		return memo[n] = (1 << (leading + 1)) - 1 - minimumOneBitOperations(n ^ (1 << leading));
+	}
+};
+// @lc code=end
