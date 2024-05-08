1ac303f545c4b5372c0f029136cf8809097919f0
lr-ch <107822234+lr-ch@users.noreply.github.com>
2024-05-08 10:41:24 +0800

    * daily challenge (2024/05/08)

---
+++ 506.relative-ranks.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=506 lang=cpp
+ *
+ * [506] Relative Ranks
+ */
+
+// @lc code=start
+class Solution {
+public:
+	vector<string> findRelativeRanks(vector<int>& score) {
+		priority_queue<pair<int, int>, vector<pair<int, int>>, less<>> pq;
+		for (int i = 0; i < score.size(); i++)
+			pq.push({ score[i], i });
+		
+		vector<string> ans(score.size());
+		int medal = 1;
+		while (!pq.empty()) {
+			auto [s, i] = pq.top(); pq.pop();
+			switch (medal) {
+				case 1 :
+					ans[i] = "Gold Medal";
+					medal++;
+					break;
+				case 2 :
+					ans[i] = "Silver Medal";
+					medal++;
+					break;
+				case 3 :
+					ans[i] = "Bronze Medal";
+					medal++;
+					break;
+				default :
+					ans[i] = to_string(medal++);
+					break;
+			}
+		}
+		return ans;
+	}
+};
+// @lc code=end
