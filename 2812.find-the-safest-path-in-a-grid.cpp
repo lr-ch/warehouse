44b55f1036ebe0a96a5f5aa04912aaa03776b6f4
lr-ch <107822234+lr-ch@users.noreply.github.com>
2024-05-15 12:33:41 +0800

    * daily challenge (2024/05/15)

---
+++ 2812.find-the-safest-path-in-a-grid.cpp
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=2812 lang=cpp
+ *
+ * [2812] Find the Safest Path in a Grid
+ */
+
+// @lc code=start
+class Solution {
+	bool isValidSF(vector<vector<int>>& g, int sf) {
+		if (g[0][0] < sf || g[N - 1][N - 1] < sf) return false;
+
+		queue<pair<int, int>> q;
+		vector<vector<bool>> visited(N, vector<bool>(N, false));
+		q.push({ 0, 0 });
+		visited[0][0] = true;
+
+		while (!q.empty()) {
+			auto [r, c] = q.front(); q.pop();
+			if (r == N - 1 && c == N - 1) return true;		// arrive destination
+
+			for (auto& [dr, dc] : dirs) {
+				int nr = r + dr, nc = c + dc;
+
+				if (nr < 0 || nr >= N || nc < 0 || nc >= N || visited[nr][nc] || g[nr][nc] < sf)
+					continue;
+
+				q.push({ nr, nc });
+				visited[nr][nc]= true;
+			}
+		}
+
+		return false;
+	}
+
+	int N;
+	const array<pair<int, int>, 4> dirs = {{ { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } }};
+public:
+	int maximumSafenessFactor(vector<vector<int>>& grid) {
+		N = grid.size();
+		queue<pair<int, int>> q;
+
+		for (int r = 0; r < N; r++)
+			for (int c = 0; c < N; c++)
+				if (grid[r][c] == 1) {
+					q.push({ r, c });
+					grid[r][c] = 0;
+				} else
+					grid[r][c] = -1;
+
+		// calculate safeness factor for each cell
+		while (!q.empty()) {
+			auto [r, c] = q.front(); q.pop();
+			for (auto& [dr, dc] : dirs) {
+				int nr = r + dr, nc = c + dc, sf = grid[r][c];
+
+				if (nr < 0 || nr >= N || nc < 0 || nc >= N || grid[nr][nc] != -1)
+					continue;
+
+				grid[nr][nc] = sf + 1;
+				q.push({ nr, nc });
+			}
+		}
+
+		// find out the maximun safeness factor
+		int max_safe = 0;
+		for_each(grid.begin(), grid.end(), [&](const auto& v) {
+			max_safe = max(max_safe, *max_element(v.begin(), v.end()));
+		});
+
+		// search a path with valid SF between 0 ~ max_safe
+		int left = 0, right = max_safe, res = 0;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+
+			if (isValidSF(grid, mid)) {
+				res = mid;
+				left = mid + 1;
+			} else
+				right = mid - 1;
+		}
+
+		return res;
+	}
+};
+// @lc code=end
