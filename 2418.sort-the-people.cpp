88c1a1f3b0de6de5b85c5a05b22422c65efc055a
lr-ch <107822234+lr-ch@users.noreply.github.com>
2024-07-22 20:19:45 +0800

    * daily challenge (2024/07/22)

---
+++ 2418.sort-the-people.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=2418 lang=cpp
+ * @lcpr version=30204
+ *
+ * [2418] Sort the People
+ */
+
+// @lc code=start
+class Solution {
+public:
+	vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+		vector<pair<int, string>> people;
+		for (int i = 0; i < names.size(); i++)
+			people.push_back({ heights[i], names[i] });
+		sort(people.rbegin(), people.rend());
+
+		vector<string> ans;
+		for (const auto& [_, name] : people) ans.push_back(name);
+		return ans;
+	}
+};
+// @lc code=end
