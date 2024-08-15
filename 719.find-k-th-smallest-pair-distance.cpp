d17bd14f4f29323fd11df454600f1335c0265cab
lr-ch <107822234+lr-ch@users.noreply.github.com>
2024-08-15 22:32:47 +0800

    * daily challenge (2024/08/14)   - Add a Binary Search + DP solution

--- 719.find-k-th-smallest-pair-distance.cpp
+++ 719.find-k-th-smallest-pair-distance.cpp
@@ -6,7 +6,7 @@
  */
 
 // @lc code=start
-class Solution {
+class SolutionBF {
 public:
 	int smallestDistancePair(vector<int>& nums, int k) {
 		sort(nums.begin(), nums.end());
@@ -23,6 +23,54 @@ public:
 				return i;
 		}
 		return 0;
+	}
+};
+
+class SolutionBinarySearchDP {
+public:
+	int smallestDistancePair(vector<int>& nums, int k) {
+		sort(nums.begin(), nums.end());
+
+		// left = the minimun distance, right = the maximun distance
+		int left = 0, right = nums.back() - nums.front();
+		while (left < right) {
+			int mid = left + (right - left) / 2, count = 0, curr = 0;
+
+			// calculate how many distances are larger than mid
+			for (int i = 0; i < nums.size(); ++i) {
+				while (curr < nums.size() && nums[i] - nums[curr] > mid) curr++;
+				count += i - curr;
+			}
+
+			if (count < k)
+				left = mid + 1;		// increase the distance
+			else
+				right = mid;		// decrease the distance
+		}
+		return right;
+	}
+};
+
+class Solution {
+	/*
+	 * Accepted
+	 *  - 19/19 cases passed (575 ms)
+	 *  - Your runtime beats 7.18 % of cpp submissions
+	 *  - Your memory usage beats 5.07 % of cpp submissions (26.2 MB)
+	 */
+	SolutionBF bf;
+
+	/*
+	 * Accepted
+	 *  - 19/19 cases passed (3 ms)
+	 *  - Your runtime beats 98.94 % of cpp submissions
+	 *  - Your memory usage beats 26.29 % of cpp submissions (13 MB)
+	 */
+	SolutionBinarySearchDP bsdp;
+public:
+	int smallestDistancePair(vector<int>& nums, int k) {
+	//	return bf.smallestDistancePair(nums, k);
+		return bsdp.smallestDistancePair(nums, k);
 	}
 };
 // @lc code=end
