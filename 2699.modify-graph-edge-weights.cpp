818cf60de83b1c4f19515d7e9bdf3d700c8bb0ba
lr-ch <107822234+lr-ch@users.noreply.github.com>
2024-08-31 01:48:25 +0800

    * daily challenge (2024/08/30)

---
+++ 2699.modify-graph-edge-weights.cpp
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=2699 lang=cpp
+ *
+ * [2699] Modify Graph Edge Weights
+ */
+
+// @lc code=start
+class Solution {
+	unordered_map<int, unordered_map<int, int>> graph;
+	unordered_map<int, unordered_map<int, bool>> modified;
+public:
+	vector<vector<int>> modifiedGraphEdges(int n, vector<vector<int>>& edges, int source, int destination, int target) {
+		// generate graph
+		for (const auto& e : edges) {
+			int a = e[0], b = e[1], w = e[2];
+			if (w == -1) {
+				modified[a][b] = true;
+				modified[b][a] = true;
+				w = 1;
+			}
+			graph[a][b] = w;
+			graph[b][a] = w;
+		}
+
+		// calculate shortest path from destination to each nodes
+		unordered_map<int, int> distFromDest;
+		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+		pq.push({ 0, destination });
+		while (!pq.empty()) {
+			auto [dist, curr] = pq.top(); pq.pop();
+			if (distFromDest.count(curr)) continue;		// already visited
+
+			distFromDest[curr] = dist;
+			for (const auto& [next, w] : graph[curr])
+				pq.push({ dist + w, next });
+		}
+
+		// calculate shortest path from source to each nodes
+		unordered_map<int, int> distFromSource;
+		pq.push({ 0, source });
+		while (!pq.empty()) {
+			auto [dist, curr] = pq.top(); pq.pop();
+			if (distFromSource.count(curr)) continue;	// already visited
+
+			distFromSource[curr] = dist;
+
+			// if the shortest path to destination is not the target
+			if (curr == destination && dist != target) return {};
+
+			for (auto [next, w] : graph[curr]) {
+				/*
+				 *   source -> ... -> curr -?-> next ... -> destination
+				 *
+				 *   Q : if "?" is modifiable, what value will it be?
+				 *   A : target - (distance between source to curr + distance between next to destination)
+				 */
+				if (modified[curr][next] && distFromSource[curr] + w + distFromDest[next] < target) {
+					w = target - (distFromSource[curr] + distFromDest[next]);
+					graph[curr][next] = w;
+					graph[next][curr] = w;
+				}
+				pq.push({ dist + w, next });
+			}
+		}
+
+		// return graph to function argument
+		for (auto& e : edges)
+			e[2] = graph[e[0]][e[1]];
+
+		return edges;
+	}
+};
+// @lc code=end
