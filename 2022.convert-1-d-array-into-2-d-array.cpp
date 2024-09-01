52b9a492199dcb07939bae4d7c914e87bcf4aba9
lr-ch <107822234+lr-ch@users.noreply.github.com>
2024-09-01 12:03:48 +0800

    * daily challenge (2024/09/01)

--- 2022.convert-1-d-array-into-2-d-array.cpp
+++ 2022.convert-1-d-array-into-2-d-array.cpp
@@ -10,7 +10,7 @@ public:
 	vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
 		if (m * n != original.size()) return {};
 
-		vector<vector<int>> res(m, vector<int>(n));
+		vector<vector<int>> res(m);
 		for (int i = 0; i < m; i++)
 			res[i] = vector<int>(original.begin() + i * n, original.begin() + i * n + n);
 		return res;
