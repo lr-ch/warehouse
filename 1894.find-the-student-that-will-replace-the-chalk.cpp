cf2591ae7bb806ef8dc97305d4563ee2205957af
lr-ch <107822234+lr-ch@users.noreply.github.com>
2024-09-02 12:17:18 +0800

    * daily challenge (2024/09/02)

---
+++ 1894.find-the-student-that-will-replace-the-chalk.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1894 lang=cpp
+ *
+ * [1894] Find the Student that Will Replace the Chalk
+ */
+
+// @lc code=start
+class Solution {
+public:
+	int chalkReplacer(vector<int>& chalk, int k) {
+		vector<unsigned long long> preSum(chalk.size());
+		preSum[0] = chalk[0];
+		for (int i = 1; i < chalk.size(); i++)
+			preSum[i] = preSum[i- 1] + chalk[i];
+
+		k %= preSum.back();
+
+		for (int i = 0; i < preSum.size(); i++)
+			if (preSum[i] > k) return i;
+
+		return INT_MAX;
+	}
+};
+// @lc code=end
+
