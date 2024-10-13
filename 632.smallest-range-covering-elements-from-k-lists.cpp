8944b074131dfe33b734d149997d324f51ddb12a
lr-ch <107822234+lr-ch@users.noreply.github.com>
2024-10-13 15:54:08 +0800

    * daily challenge (2024/10/13)

--- 632.smallest-range-covering-elements-from-k-lists.cpp
+++ 632.smallest-range-covering-elements-from-k-lists.cpp
@@ -9,27 +9,33 @@ class Solution {
 public:
 	vector<int> smallestRange(vector<vector<int>>& nums) {
 		// (value, group id, pointer to next)
-		set<tuple<int, int, int>> candidates;
+		priority_queue<
+			tuple<int, int, int>,
+			vector<tuple<int, int, int>>,
+			greater<tuple<int, int, int>>> candidates;
+		int value_to = INT_MIN;
 
 		// for each group, insert the first element into candidate
-		for (int i = 0; i < nums.size(); i++)
-			candidates.insert({nums[i][0], i, 1});
+		for (int i = 0; i < nums.size(); i++) {
+			candidates.push({nums[i][0], i, 1});
+			value_to = max(value_to, nums[i][0]);
+		}
 
 		vector<int> result = {0, INT_MAX};
 		while (true) {
-			auto [value_from, groupid_min, pointer_min] = *candidates.begin();
-			auto [value_to, groupid_max, pointer_max] = *candidates.rbegin();
+			auto [value_from, groupid, pointer] = candidates.top();
+			candidates.pop();
 
-			if (value_to - value_from < result[1] - result[0]) {
-				result[0] = value_from;
-				result[1] = value_to;
-			}
+			if (value_to - value_from < result[1] - result[0])
+				result = {value_from, value_to};
 
 			// update the smallest candidate to its group's next element
-			candidates.erase(candidates.begin());
-			if (pointer_min < nums[groupid_min].size())
-				candidates.insert({nums[groupid_min][pointer_min], groupid_min, pointer_min + 1});
-			else
+			if (pointer < nums[groupid].size()) {
+				candidates.push({nums[groupid][pointer], groupid, pointer + 1});
+
+				// update value_to if new candidate is larger
+				value_to = max(value_to, nums[groupid][pointer]);
+			} else
 				// stop if no more smallest element can join candidates
 				break;
 		}
